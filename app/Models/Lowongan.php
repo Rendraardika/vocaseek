4a21e2605e39064a204f5aea5dca3028ded0c373
rendra ardika <ardikarendra50@gmail.com>
2026-04-10 15:35:15 +0700

    up

--- app/Models/Lowongan.php
+++ app/Models/Lowongan.php
@@ -11,14 +11,19 @@ class Lowongan extends Model
 
     protected $table = 'lowongan'; // Karena nama tabel kita 'lowongan'
 
-   protected $fillable = [
-    'company_profile_id', 
-    'judul_posisi',
-    'deskripsi_pekerjaan',
-    'persyaratan',
-    'lokasi',
-    'tipe_magang',
-    'gaji_per_bulan',
-    'status'
-];
+    protected $fillable = [
+        'company_profile_id',
+        'judul_posisi',
+        'deskripsi_pekerjaan',
+        'persyaratan',
+        'lokasi',
+        'tipe_magang',
+        'gaji_per_bulan',
+        'status'
+    ];
+
+    public function companyProfile()
+    {
+        return $this->belongsTo(CompanyProfile::class, 'company_profile_id', 'id');
+    }
 }
