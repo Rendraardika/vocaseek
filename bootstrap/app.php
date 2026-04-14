698d40f7e6fd8a55db9a95a35e7afe54b829602d
rendra ardika <ardikarendra50@gmail.com>
2026-04-14 10:37:02 +0700

    oush

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -44,6 +44,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'api/auth/google',
             'api/auth/google/callback',
             'api/company/jobs', 
+            'api/admin/users-management*',
             'api/admin/verification/*', // Tambahkan ini agar verifikasi admin lancar
         ]);
     })
